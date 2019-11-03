2e162aed8c2942c21bfcb2bacab16be023a7390c
Michael Senn <michael@morrolan.ch>
2019-11-03 16:46:13 +0100

    Further work on final report.

--- experiments/2_vonballmoos/doc/r/bca_assay.R
+++ experiments/2_vonballmoos/doc/r/bca_assay.R
@@ -5,6 +5,13 @@ library(scales)
 
 df = read.csv("../data/bca_assay.csv")
 
+min_max_standard_abs = df %>% 
+  filter(type == 'standard') %>% 
+  summarize(min = min(absorption), max = max(absorption))
+
+df %>% 
+  filter(type == 'sample' & between(absorption, min_max_standard_abs$min, min_max_standard_abs$max))
+
 ggplot(df, aes(x = concentration, y = absorption, color = type)) +
   geom_point() +
   stat_function(fun=function(x)0.81542*x + 0.02701, geom="line", aes(colour="0.81542 * x + 0.02701")) +
--- experiments/2_vonballmoos/doc/r/expression_screening.R
+++ experiments/2_vonballmoos/doc/r/expression_screening.R
@@ -15,16 +15,20 @@ df = df %>% filter(time != 90)
 ggplot(df, aes(x = time, y = od, color = rha:iptg)) +
   geom_point() +
   geom_line() +
+  geom_vline(xintercept = 90, linetype = 'dotted') +
+  annotate("text", x = 90, y = 1.5, label = "Addition of IPTG") +
   ylim(0, 1.6) +
-  labs(title = "OD600 measurement of E. coli cells under various conditions", x = "Time since inoculation [min]", y = "OD600") +
+  labs(title = "OD600 measurement of E. coli cells during expression with various conditions", x = "Time since inoculation [min]", y = "OD600") +
   facet_wrap(vars(medium), nrow = 3) +
-  theme_minimal()
+  theme_minimal() +
+  ggsave('../img/absorption_expression.png')
 
 df = read.csv("../data/expression_fluorescence.csv")
 
 # Fluorescence measurements
 ggplot(df, aes(x = Medium, y = Fluorescence.Normalized, fill = Rhamnose:IPTG)) +
   geom_col(position = position_dodge()) +
-  labs(title = "Fluorescence of E. coli cells under various conditions", x = "", y = "Normalized fluorescence") + 
-  theme_minimal()
+  labs(title = "Fluorescence of E. coli cells after expression with various conditions", x = "", y = "Normalized fluorescence") + 
+  theme_minimal() +
+  ggsave('../img/expression_fluorescence.png')
 
