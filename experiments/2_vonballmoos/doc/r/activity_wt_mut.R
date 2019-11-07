9ded7deb45b9593444daebe9382938b227847cda
Michael Senn <michael@morrolan.ch>
2019-11-07 18:42:24 +0100

    Further work on report.

--- experiments/2_vonballmoos/doc/r/activity_wt_mut.R
+++ experiments/2_vonballmoos/doc/r/activity_wt_mut.R
@@ -17,6 +17,7 @@ mut.df_slopes_wide$relative_reduction = 1 - mut.df_slopes_wide$HS / (mut.avg_xo_
 mut.df_slopes = mut.df_slopes_wide %>% filter(sample != 'SOD')
 mut.df_slopes$concentration_inverse = 1 / mut.df_slopes$concentration
 mut.df_slopes$relative_reduction_inverse = 1 / mut.df_slopes$relative_reduction
+mut.df_slopes$relative_reduction_vs_conc = mut.df_slopes$relative_reduction / mut.df_slopes$concentration
 
 
 wt.df_slopes_long = read.csv('../data/raw/activity_wt_quinone_slope.csv')
@@ -26,6 +27,7 @@ wt.sod_slope = (wt.df_slopes_wide %>% filter(sample == 'SOD'))$HS
 wt.avg_xo_slope = (wt.df_slopes_wide %>% filter(sample != 'SOD') %>% summarize(avg=mean(XO)))$avg
 # We want to know how much the reduction of the dye was quenched, as this corresponds with the enzymatic activity.
 wt.df_slopes_wide$relative_reduction = 1 - wt.df_slopes_wide$HS / (wt.avg_xo_slope - wt.sod_slope)
+wt.df_slopes$relative_reduction_vs_conc = wt.df_slopes$relative_reduction / wt.df_slopes$concentration
 
 wt.df_slopes = wt.df_slopes_wide %>% filter(sample != 'SOD')
 wt.df_slopes$concentration_inverse = 1 / wt.df_slopes$concentration
@@ -65,4 +67,19 @@ ggplot(df_slopes, aes(x = concentration_inverse, y = relative_reduction_inverse,
     legend.title = element_text(size = rel(1.2))
   ) +
   ggsave('../img/activity_km_lb.png', width = 20, units = 'cm', dpi = 'print')
+
+ggplot(df_slopes, aes(x = relative_reduction, y = relative_reduction_vs_conc, color = protein)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE) +
+  stat_regline_equation(
+    aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~"))
+  ) +
+  facet_wrap(vars(protein), scales = 'free', ncol = 1) +
+  labs(title = "Determination of HS KM", x = "", y = "") +
+  theme_minimal() +
+  theme(
+    legend.text = element_text(size = rel(1.2)),
+    legend.title = element_text(size = rel(1.2))
+  ) +
+  ggsave('../img/activity_km_lb_alternative.png', width = 20, units = 'cm', dpi = 'print')
 
